0d01e580f811775b277e072724453649bc215ff0
Bahaa Zenhom <bahaazenhom7@gmail.com>
2025-11-02 11:25:07 +0200

    fix: update Swagger server URL to production endpoint

--- src/config/swagger.config.js
+++ src/config/swagger.config.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:5000',
+        url: 'https://fullsnack.obl.ee',
         description: 'Development server',
       },
     ],
